293daab9a6b41dc541fb00175f11aa858d6e4ee9
Nadont02 <dunyushkin2007@mail.ru>
2026-01-19 13:22:06 +0300

    Added examples of bullet, numbered and nested lists

---
+++ program/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Напишите два числа:");
+Console.WriteLine("Первое число:");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Второе число:");
+double n2 = Convert.ToDouble(Console.ReadLine());
+double sum = n1 + n2;
+Console.WriteLine($"**Результат: {sum}**");
